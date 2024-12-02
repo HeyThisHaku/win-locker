c80c250eda4dbd3d2d8842c0dae01e3f5f6aeb55
HeyThisHaku <sandikalawlet@gmail.com>
2024-12-02 17:15:39 +0700

    feat: fix unlocker bugs

--- locker.py
+++ locker.py
@@ -1,6 +1,5 @@
 from pnp_controller import lock_mouse,lock_keyboard
 from tkinter import *
-import pyfiglet
 import os
 import sys
 
@@ -16,6 +15,10 @@ text = """
  YOUR KEYBOARD AND MOUSE ARE LOCKED
 """
 
+PID_VALUE = os.getpid()
+PID_NAME = "lock.pid"
+TEMP_PATH = "%temp%"
+
 def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
@@ -28,8 +31,12 @@ def show_ui():
     print(text)
     input("Press enter for close this prompt, Tools Credit by Haku, Maintain w/ NA")
 
+def save_pid():
+    os.system("echo {} > {}\{}".format(PID_VALUE,TEMP_PATH,PID_NAME))
+
 
 if __name__ == "__main__":
     lock_keyboard()
-    lock_mouse()
+    # lock_mouse()
+    save_pid()
     show_ui()
--- pnp_controller.py
+++ pnp_controller.py
@@ -1,7 +1,32 @@
 from pnputil import get_devices,enable_devices,disable_devices
 import keyboard
+import os
+import signal
 
+PID_NAME = "lock.pid"
+TEMP_PATH = os.getenv("TEMP", "")
 
+def read_pid_file():
+    pid_file_path = os.path.join(TEMP_PATH, PID_NAME) 
+    if not os.path.isfile(pid_file_path):
+        raise FileNotFoundError(f"The file {pid_file_path} does not exist.")
+    
+    with open(pid_file_path, "r") as file:
+        content = file.read()
+    
+    return content
+
+def delete_pid_file():
+    if not TEMP_PATH:
+        raise EnvironmentError("TEMP path is not set.")
+    
+    pid_file_path = os.path.join(TEMP_PATH, "lock.pid")
+    
+    if os.path.isfile(pid_file_path):
+        os.remove(pid_file_path) 
+        print(f"File {pid_file_path} has been deleted.")
+    else:
+        raise FileNotFoundError(f"The file {pid_file_path} does not exist.")
 
 def lock_mouse():
     get_mouses = get_devices(class_name="Mouse")
@@ -19,4 +44,10 @@ def lock_keyboard():
 
 def unlock_keyboard():
     for i in range(150):
-        keyboard.unblock_key(i)
+        try:
+            keyboard.unblock_key(i)
+        except:
+            pass
+    # Kill LAST CMD
+    os.kill(int(read_pid_file()), signal.SIGTERM)
+    delete_pid_file()
